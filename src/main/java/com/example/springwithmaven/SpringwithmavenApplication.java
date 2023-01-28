e97693102fe1e215c78f08dc1f4c33a19963963d
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2023-01-28 08:55:23 +0200

    init

---
+++ src/main/java/com/example/springwithmaven/SpringwithmavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwithmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithmavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwithmaven/SpringwithmavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwithmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithmavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
